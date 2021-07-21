2cfa0637088242ec3a38bdeb27a7913884e7d53c
thetrucy <86333597+thetrucy@users.noreply.github.com>
2021-07-21 21:16:35 +0700

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,3 +1,11 @@
-h = 5
-for i in range(h+1):
-    print ('* ' * i)
+h= int(input('Enter height: '))
+for i in range(2*h-1,0,-1):
+    print(' * ',end=' ')
+print()
+for i in range(h-1,0,-1):
+    for n in range(2*h-1):
+        if n == h+i-2 or n == h-i :
+            print(' * ',end=' ')
+        else: 
+            print('  ',end='  ')
+    print()
